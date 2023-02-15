0bf8b742852aed1893542adeb841cef7a1ce2fa7
vandriodd <ludasaadi@gmail.com>
2023-02-15 08:35:17 -0800

    HelloPutFix?

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,8 @@
-/* This program prints a string */
-
 #include <stdio.h>
+
+/* This program prints a string */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
-	return 0;
+	return (0);
 }
