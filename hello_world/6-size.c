308aefa074ec44856744508ed5cb3115cb4bb79e
vandriodd <ludasaadi@gmail.com>
2023-02-15 09:23:38 -0800

    sizeofIx3

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -8,10 +8,10 @@
 
 int main(void)
 {
-	printf("Size of a char: %zu bytes(s)\n", sizeof(char));
-	printf("Size of an int: %zu bytes(s)\n", sizeof(int));
-	printf("Size of a long int: %zu bytes(s)\n", sizeof(long int));
-	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+	printf("Size of a char: %li bytes(s)\n", sizeof(char));
+	printf("Size of an int: %li bytes(s)\n", sizeof(int));
+	printf("Size of a long int: %li bytes(s)\n", sizeof(long int));
+	printf("Size of a long long int: %li byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %li byte(s)\n", sizeof(float));
 	return (0);
 }
