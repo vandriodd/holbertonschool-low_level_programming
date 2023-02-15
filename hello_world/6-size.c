4ea8aa416d2208610fc22733d5a6f2e3243bb2d5
vandriodd <ludasaadi@gmail.com>
2023-02-15 09:26:42 -0800

    sizeofIxf

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -8,9 +8,9 @@
 
 int main(void)
 {
-	printf("Size of a char: %li bytes(s)\n", sizeof(char));
-	printf("Size of an int: %li bytes(s)\n", sizeof(int));
-	printf("Size of a long int: %li bytes(s)\n", sizeof(long int));
+	printf("Size of a char: %li byte(s)\n", sizeof(char));
+	printf("Size of an int: %li byte(s)\n", sizeof(int));
+	printf("Size of a long int: %li byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %li byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %li byte(s)\n", sizeof(float));
 	return (0);
