dc8102189993c55fc84e341abdd8afeb0a13c17e
vandriodd <ludasaadi@gmail.com>
2023-02-15 08:36:54 -0800

    HelloPutagain

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -3,6 +3,6 @@
 /* This program prints a string */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
