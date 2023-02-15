1d9b30c9873f257a681bb7759ba70c52ccedc80e
vandriodd <ludasaadi@gmail.com>
2023-02-15 09:15:33 -0800

    sizeofIx

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
 /**
- * main - this program prints the size of various types 
+ * main - this program prints the size of various types
  *
  * Return: Always 0
  */
 
 int main(void)
 {
-	printf("Size of a char \: %ld bytes(s)\n", sizeof(char));
-	printf("Size of an int \: %ld bytes(s)\n", sizeof(int));
-	printf("Size of a long int \: %ld bytes(s)\n", sizeof(long int));
-	printf("Size of a long long int \: %ld byte(s)\n", sizeof(long long int));
-	printf("Size of a float \: %ld byte(s)\n", sizeof(float));
+	printf("Size of a char: %ld bytes(s)\n", sizeof(char));
+	printf("Size of an int: %ld bytes(s)\n", sizeof(int));
+	printf("Size of a long int: %ld bytes(s)\n", sizeof(long int));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %ld byte(s)\n", sizeof(float));
 	return (0);
 }
