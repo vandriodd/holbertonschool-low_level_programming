45fa7671c730be25f4c5f4eaa80d43f31f68ac79
vandriodd <ludasaadi@gmail.com>
2023-02-16 11:38:05 -0800

    mainComm

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -3,34 +3,34 @@
 /* */
 int _putchar(char c);
 
-/* */
+/* Task 1 -  I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game */
 void print_alphabet(void);
 
-/* */
+/* Task 2 - 10 x alphabet */
 void print_alphabet_x10(void);
 
-/* */
+/* Task 3 - islower */
 int _islower(int c);
 
-/* */
+/* Task 4 - isalpha */
 int _isalpha(int c);
 
-/* */
+/* Task 5 - Sign */
 int print_sign(int n);
 
-/* */
+/* Task 6 - There is no such thing as absolute value in this world. You can only estimate what a thing is worth to you */
 int _abs(int);
 
-/* */
+/* Task 7 - There are only 3 colors, 10 digits, and 7 notes; it's what we do with them that's important */
 int print_last_digit(int);
 
-/* */
+/* Task 8 -  I'm federal agent Jack Bauer, and today is the longest day of my life */
 void jack_bauer(void);
 
-/* */
+/* Task 9 - Learn your times table */
 void times_table(void);
 
-/*  */
+/* Task 10 - a + b */
 int add(int, int);
 
 /* Task 11 - 98 Battery Street, the OG */
