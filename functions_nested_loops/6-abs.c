ece5cb4d4019cb67f5f3786cd2197ff45c0ccba7
vandriodd <ludasaadi@gmail.com>
2023-02-17 05:33:01 -0800

    absInt

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - absolute value
+ * @n: integer
+ * Return: the absolute value of a number
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n = n * -1;
+		return (n);
+	}
+	else
+	{
+		return (n);
+	}
+}
