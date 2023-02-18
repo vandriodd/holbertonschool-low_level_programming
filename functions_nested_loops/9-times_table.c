3c0a902b5d7f1b2645615a93e2e26097bd7ab314
vandriodd <ludasaadi@gmail.com>
2023-02-18 06:19:43 -0800

    9 times table

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table starting with 0
+ * @x,y: axis
+ * @i,n: integers
+ * Return: 9 times table
+ */
+
+void times_table(void)
+{
+	int x, y, r;
+
+	for (y = 0; y <= 9; y++)
+	{
+		for (x = 0; x <= 9; x++)
+		{
+			r = y * x;
+
+			if (r > 9)
+			{
+				_putchar(' ');
+			}
+			else if (x != 0)
+			{
+				_putchar(' ');
+				_putchar(' ');
+			}
+			_putchar('0' + r);
+
+
+			if (x < 9)
+			{
+				_putchar(',');
+			}
+		}
+		_putchar('\n');
+	}
+}
