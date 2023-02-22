1277c65c3f1e993e68c13e278177eada4f452568
vandriodd <ludasaadi@gmail.com>
2023-02-22 09:43:12 -0800

    main

---
+++ more_functions_nested_loops/main.h
@@ -0,0 +1,33 @@
+#include <unistd.h>
+
+int _putchar(char c);
+
+/* Task 0 - isupper */
+int _isupper(int c);
+
+/* Task 1 - isdigit */
+int _isdigit(int c);
+
+/* Task 2 - Collaboration is multiplication */
+int mul(int a, int b);
+
+/* Task 3 - The numbers speak for themselves */
+void print_numbers(void);
+
+/* Task 4 - I believe in numbers and signs */
+void print_most_numbers(void);
+
+/* Task 5 - Numbers constitute the only universal language*/
+void more_numbers(void);
+
+/* Task 6 - The shortest distance between two points is a straight line*/
+void print_line(int n);
+
+/* Task 7 - I feel like I am diagonally parked in a parallel universe */
+void print_diagonal(int n);
+
+/* Task 8 - You are so much sunshine in every square inch */
+void print_square(int size);
+
+/* Task 10 - Triangles */
+void print_triangle(int size);
