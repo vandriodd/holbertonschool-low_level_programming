4c0bda1cc4b95d0f504302e6871c428c2a9e2615
vandriodd <ludasaadi@gmail.com>
2023-02-22 10:09:24 -0800

    collabMul

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - multiplies two integers
+ * @a: first number
+ * @b: second number
+ * Return: multiplication a & b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
