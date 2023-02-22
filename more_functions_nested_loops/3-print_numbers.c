faf7b4d263e5651416eb2c8c9b19f23b13ffe916
vandriodd <ludasaadi@gmail.com>
2023-02-22 10:14:06 -0800

    numberSpeak

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers from 0 to 9
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int c = 0;
+
+	while (c <= '9')
+	{
+		_putchar('0' + c);
+		c++;
+	}
+	_putchar('\n');
+}
