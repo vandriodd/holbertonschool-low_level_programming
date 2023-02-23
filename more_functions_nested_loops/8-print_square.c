6aa0c3d0aa367636858985c74372e8fc032d08dd
vandriodd <ludasaadi@gmail.com>
2023-02-23 04:10:25 -0800

    task 8

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square
+ * @size: determines the size of the square with #
+ * Return: void
+ */
+
+void print_square(int size)
+{
+	int l, i;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (l = 0; l < size; l++)
+		{
+			for (i = 0; l < size; i++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
