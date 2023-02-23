1e775292bd440cac93d98b7d2f40b6368bddfa2c
vandriodd <ludasaadi@gmail.com>
2023-02-22 16:53:09 -0800

    task 5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times numbers from 0 to 14
+ *
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int c, d, i;
+
+	for (c = 0; c < 10; c++)
+	{
+		for (d = 0; d < 15; d++)
+		{
+			if (d < 10)
+			{
+				i = d;
+			}
+			else
+			{
+				i = d / 10;
+			}
+
+			_putchar('0' + i);
+
+			if (d > 9)
+				_putchar('0' + (d % 10));
+		}
+	}
+
+	_putchar('\n');
+}
