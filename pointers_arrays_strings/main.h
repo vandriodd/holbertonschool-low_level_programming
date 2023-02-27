88fe2e2a721badb1f98528fb3d0e50d722078fe5
vandriodd <ludasaadi@gmail.com>
2023-02-27 06:44:50 -0800

    main

---
+++ pointers_arrays_strings/main.h
@@ -0,0 +1,36 @@
+#include <unistd.h>
+
+int _putchar(char c);
+
+/* Task 0 - 98 Battery st. */
+void reset_to_98(int *n);
+
+/* Task 1 - Don't swap horses in crossing a stream */
+void swap_int(int *a, int *b);
+
+/* Task 2 - This report, by its very length, defends itself against the risk of being read */
+int _strlen(char *s);
+
+/* Task 3 - I do not fear computers. I fear the lack of them */
+void _puts(char *str);
+
+/* Task 4 - I can only go one way. I've not got a reverse gear */
+void print_rev(char *s);
+
+/* Task 5 - A good engineer thinks in reverse and asks himself about the stylistic consequences of the components and systems he proposes */
+void rev_string(char *s);
+
+/* Task 6 - Half the lies they tell about me aren't true */
+void puts2(char *str);
+
+/* Task 7 - Winning is only half of it. Having fun is the other half */
+void puts_half(char *str);
+
+/* Task 8 - Arrays are not pointers */
+void print_array(int *a, int n);
+
+/* Task 9 - strcpy */
+char *_strcpy(char *dest, char *src);
+
+/* Task 10 - Great leaders are willing to sacrifice the numbers to save the people. Poor leaders sacrifice the people to save the numbers */
+int _atoi(char *s);
