58b512e90ae54fe2ccdad7ab572291da8dfe1b15
vandriodd <ludasaadi@gmail.com>
2023-03-01 05:19:46 -0800

    update main

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -34,3 +34,29 @@ char *_strcpy(char *dest, char *src);
 
 /* Task 10 - Great leaders are willing to sacrifice the numbers to save the people. Poor leaders sacrifice the people to save the numbers */
 int _atoi(char *s);
+
+/* >> 2nd Project << */
+
+/* Task 0 - strcat */
+char *_strcat(char *dest, char *src);
+
+/* Task 1 - strncat */
+char *_strncat(char *dest, char *src, int n);
+
+/* Task 2 - strncpy */
+char *_strncpy(char *dest, char *src, int n);
+
+/* Task 3 - strcmp */
+int _strcmp(char *s1, char *s2);
+
+/* Task 4 - I am a kind of paranoid in reverse. I suspect people of plotting to make me happy */
+void reverse_array(int *a, int n);
+
+/* Task 5 - Always look up */
+char *string_toupper(char *);
+
+/* Task 6 - Expect the best. Prepare for the worst. Capitalize on what comes */
+char *cap_string(char *);
+
+/* Task 7 - Mozart composed his music not for the elite, but for everybody */
+char *leet(char *);
