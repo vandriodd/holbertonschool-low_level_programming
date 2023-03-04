534e72e23f1a5f524378be7520cd39115aaf0080
vandriodd <ludasaadi@gmail.com>
2023-03-04 07:18:15 -0800

    strstrFix

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strstr - locates a substring
+ * @haystack: string from where substring will be searched
+ * @needle: substring to be searched
+ * Return: pointer to the first occurrence of the matched string or NULL if the substring is not found
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i, t;
+
+	if (needle[0] == '\0')
+		return (haystack);
+
+	for (i = 0; haystack[i] - 1; i++) /* -1 avoid segmentation fault */
+	{
+		if (haystack[i] == needle[0])
+		{
+			for (t = 0; needle[t] - 1; t++)
+			{
+				if (haystack[i + t] != needle[t])
+					break;
+
+				if (needle[t] == '\0')
+					return (haystack + i);
+			}
+		}
+	}
+
+	return ('\0');
+}
