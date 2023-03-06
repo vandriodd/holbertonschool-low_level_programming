19da9c3763f355f46edb9c034ced0cce46ce847e
vandriodd <ludasaadi@gmail.com>
2023-03-06 04:48:03 -0800

    recursionDir

---
+++ recursion/main.h
@@ -0,0 +1,24 @@
+#include <unistd.h>
+
+int _putchar(char c);
+
+/* Task 0 - She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget */
+void _puts_recursion(char *s);
+
+/* Task 1 - Why is it so important to dream? Because, in my dreams we are together */
+void _print_rev_recursion(char *s);
+
+/* Task 2 - Dreams feel real while we're in them. It's only when we wake up that we realize something was actually strange */
+int _strlen_recursion(char *s);
+
+/* Task 3 - You mustn't be afraid to dream a little bigger, darling */
+int factorial(int n);
+
+/* Task 4 - Once an idea has taken hold of the brain it's almost impossible to eradicate */
+int _pow_recursion(int x, int y);
+
+/* Task 5 - Your subconscious is looking for the dreamer */
+int _sqrt_recursion(int n);
+
+/* Task 6 - Inception. Is it possible? */
+int is_prime_number(int n);
