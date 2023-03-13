d0f6b665d34642652124b2bd3f4a879211eb1362
vandriodd <ludasaadi@gmail.com>
2023-03-13 10:01:07 -0700

    main

---
+++ malloc_free/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* libraries */
+#include <stdio.h>
+#include <stdlib.h>
+
+/* prototypes */
+/* Task 0 */ char *create_array(unsigned int size, char c);
+/* Task 1 */ char *_strdup(char *str);
+/* Task 2 */ char *str_concat(char *s1, char *s2);
+/* Task 3 */ int **alloc_grid(int width, int height);
+/* Task 4 */ void free_grid(int **grid, int height);
+
+#endif
