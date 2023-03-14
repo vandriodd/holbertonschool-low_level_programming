cf61f3c79eda61df0af5a824b658ca2341374bb5
vandriodd <ludasaadi@gmail.com>
2023-03-14 11:42:48 -0700

    more malloc

---
+++ more_malloc_free/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* libraries */
+#include <stdio.h>
+#include <stdlib.h>
+
+/* prototypes */
+/* Task 0 */ void *malloc_checked(unsigned int b);
+/* Task 1 */ char *string_nconcat(char *s1, char *s2, unsigned int n);
+/* Task 2 */ void *_calloc(unsigned int nmemb, unsigned int size);
+/* Task 3 */ int *array_range(int min, int max);
+
+#endif
