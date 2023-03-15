d7afaba0f42ba31fe96693a26e19e2c2fc5f08cd
vandriodd <ludasaadi@gmail.com>
2023-03-14 17:20:21 -0700

    mallCheck

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: memory size to allocate
+ * Return: pointer to the allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+
+	if (!b)
+		exit(98);
+
+	return (p);
+}
