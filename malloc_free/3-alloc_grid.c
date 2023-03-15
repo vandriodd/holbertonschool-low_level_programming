37bad47e776d3fcd8c59a81582c7fe64a06c606d
vandriodd <ludasaadi@gmail.com>
2023-03-15 05:21:09 -0700

    fix?

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -16,7 +16,7 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	grid = (int **)malloc(sizeof(int) * height);
+	grid = malloc(sizeof(int *) * height);
 
 	/* in case of failure return NULL */
 	if (!grid)
@@ -24,7 +24,7 @@ int **alloc_grid(int width, int height)
 
 	for (i = 0; i < height; i++)
 	{
-		grid[i] = (int *)malloc(sizeof(int) * width);
+		grid[i] = malloc(sizeof(int) * width);
 
 		/* in case of the mem is not enough */
 		if (!grid[i])
