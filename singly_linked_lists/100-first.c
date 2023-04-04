d1c3d5940a277c6bab2472504cef0d11002fa493
vandriodd <ludasaadi@gmail.com>
2023-04-04 15:39:42 -0700

    [Advanced task] review

---
+++ singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * print_first - (constructor function),
+ * prints something before the main function
+ */
+void print_first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
