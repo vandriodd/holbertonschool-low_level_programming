c1c76051819ba24de101ed1370f2984f3866555a
vandriodd <ludasaadi@gmail.com>
2023-04-04 06:56:32 -0700

    [Update task 2] aux prototypes added

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -25,6 +25,8 @@ int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
+int _strlen(const char *str);
+char *_strdup(const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
