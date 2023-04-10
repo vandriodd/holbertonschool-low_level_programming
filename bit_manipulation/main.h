e4e75a29f7064e735c3b33bcc341f6ef25031e6a
vandriodd <ludasaadi@gmail.com>
2023-04-09 20:16:04 -0700

    [Update task 0] aux function prototype

--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -8,6 +8,7 @@
 /* Prototypes */
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
+unsigned int power(int x, int y);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
